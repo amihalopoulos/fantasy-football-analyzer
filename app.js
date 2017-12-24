94c70d80852e8943b2b4046778ab6aaa55cde8f5
Alexei <alexander_mihalopoulos@comcast.com>
2017-12-24 10:59:07 -0500

    progress

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var mongoose = require('mongoose');
 var api = require('./api');
+var _ = require('underscore');
 var app;
 
 var thisAPI = new api()
@@ -51,22 +52,6 @@ var redirectUri = process.env.REDIRECT_URI || 'http://alexei.com/auth/yahoo/call
     resave: false,
     saveUninitialized: true
   }));
-
-  // app.use(function(req, res, next) {
-  //   if (req.session && req.session.user) {
-  //     User.findOne({ guid: req.session.user.guid }, function(err, user) {
-  //       if (user) {
-  //         req.user = user;
-  //         req.session.user = user;  //refresh the session value
-  //         res.locals.user = user;
-  //       }
-  //       // finishing processing the middleware and run the route
-  //       next();
-  //     });
-  //   } else {
-  //     next();
-  //   }
-  // });
 
   app.use(express.static(path.join(__dirname, 'app')));
 
@@ -160,44 +145,122 @@ var redirectUri = process.env.REDIRECT_URI || 'http://alexei.com/auth/yahoo/call
     });
   });
 
-  app.get('/games', function(req, res){
+  app.get('/league/:leagueKey', function(req, res){
     if (!req.session.user) {
       return res.redirect('/auth/yahoo')
     }
     var user = req.session.user;
-    var leaguesOptions = {
-      url: 'https://fantasysports.yahooapis.com/fantasy/v2/games?format=json',
+    console.log(req.params)
+
+    // settings: "http://fantasysports.yahooapis.com/fantasy/v2/league/223.l.431/settings"
+    // standings: https://fantasysports.yahooapis.com/fantasy/v2/league/223.l.431/standings
+
+    //get every single roster
+
+    var opts = {
+      url: 'https://fantasysports.yahooapis.com/fantasy/v2/team/'+req.params.leagueKey+'.t.10/roster/players?format=json',
       method: 'GET',
       headers: { Authorization: 'Bearer ' + user.accessToken },
       rejectUnauthorized: false,
       json: true
     }
 
-    request(leaguesOptions, function(err, response, body){
-      if (err) {
-        console.log( err )
-      } else {
-        if (body.error) {
-          console.log(body.error)
-        }
-        var gamesData = body.fantasy_content.games['0']
+    request(opts, function(err, response, body){
+      //team[1].roster['0'].players['0'].player[0].name
 
-        res.render('games', {
-          title: 'Games',
-          user: req.session.user,
-          games: gamesData.game[0].name
-        })
+      //normalize rosters
+      var normalized = [];
+      var final = [];
+      var rawRoster = body.fantasy_content.team[1].roster['0'].players;
+      console.log(rawRoster)
+      var numPlayers = Object.keys(rawRoster)
+      for (var i = 0; i < numPlayers.length; i++) {
+        var x = ''+i
+        if (rawRoster[x] && rawRoster[x].player) {
+          normalized.push(rawRoster[x].player)
+        }
       }
+      for (var i = 0; i < normalized.length; i++) {
+        var player = {}
+        player.name = normalized[i][0][2].name
+        // final.push(normalized[i][0])
+        final.push(player)
+
+      }
+console.log(final)
+
+
+      var roster = body.fantasy_content.team
+
+      res.render('league', {
+        title: 'League',
+        user: req.session.user,
+        roster: final
+      })
     })
 
+
+  })
+
+
+  app.get('/games', function(req, res){
+    if (!req.session.user) {
+      return res.redirect('/auth/yahoo')
+    }
+    var user = req.session.user;
+    var leaguesOptions = {
+      url: 'https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=371/leagues?format=json',
+      method: 'GET',
+      headers: { Authorization: 'Bearer ' + user.accessToken },
+      rejectUnauthorized: false,
+      json: true
+    }
+
+    if (user && user.leagues) {
+
+      res.render('games', {
+        title: 'Games',
+        user: req.session.user,
+        games: user.leagues
+      })
+
+    } else {
+
+      request(leaguesOptions, function(err, response, body){
+        if (err) {
+          console.log( err )
+        } else {
+          if (body.error) {
+            console.log(body.error)
+          }
+          var leagues = body.fantasy_content.users['0'].user[1].games['0'].game[1].leagues[0].league;
+
+          user = req.session.user;
+
+          User.findOne({ guid: user.guid }, function(err, existingUser) {
+            if (existingUser) {
+              existingUser.leagues = leagues
+              existingUser.save(function(err){
+                console.log(existingUser)
+              })
+            }
+          })
+
+
+          res.render('games', {
+            title: 'Games',
+            user: req.session.user,
+            games: leagues
+          })
+        }
+      })
+    }
   });
 
   app.get('/logout', function(req, res) {
     delete req.session.user;
     res.redirect('/');
   });
-
-// configureApplication()
 
 app.listen(app.get('port'), function() {
   console.log('app listening on port ' + app.get('port'))
